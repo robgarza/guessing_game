42ca369e92de19c4b1266d6600f13bfb42cd91de
Robert Garza <rob@Roberts-MacBook-Air.local>
2021-03-26 09:16:37 -0400

    edit correct answer, update game logic.

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,7 +1,7 @@
 function guessinggame {
 echo "Guess how many files are in the current directory (enter an integer please): "
 input=""
-while [[ $input -ne 2 ]]
+while [[ $input -ne 3 ]]
 do
 read input
 if [[ $input ]] && [ $input -eq $input 2>/dev/null ]
@@ -9,13 +9,15 @@ then
   if [[ $input -eq 3 ]]
   then
     echo "Correct!"
+   elif [[ $input < 3 ]]
+    then 
+    echo "Too low. Guess again!"
    else
-     echo "Guess again!"
+    echo "Too high. Guess again!"
    fi
 elif [[ "$input" =~ [\.] ]]
 then
   echo "No decimals please. Run bash guessinggame.sh in your terminal to play the game again!"
-  exit 0
 else
   echo "Please enter an integer"
 fi
